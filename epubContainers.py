de4a715726792d615fc8e9a0dab0a37c4749acff
DESKTOP-R90IN5C\benja <benjaminkim797@gmail.com>
2017-12-14 20:36:03 -0600

    Added some functionality and organize folder structure

--- epubContainers.py
+++ epubContainers.py
@@ -1,13 +1,14 @@
 import xml.etree.ElementTree as ET
-import datetime
 
+#create an etree Element
 def createElement(name,params={},text=''):
 	element = ET.Element(name,params)
 	element.text = text
 	return element
 		
+#structure for content.opf file
 class opf:
-
+	#creates skeleton structure for opf.  Still need to add title,identifier,languge, and update date to fit minimum requirements
 	def __init__(self):
 		self.package = self.makePackage()
 		self.meta = self.makeMetaData()
@@ -15,37 +16,49 @@ class opf:
 		self.spine = ET.Element("spine")
 		self.package.extend([self.meta,self.manifest,self.spine])
 
+	#create package element
 	def makePackage(self):
 		docType = {"xmlns":"http://www.idpf.org/2007/opf","version":"3.0","xml:lang":"en","unique-identifier":"bookid"}
 		return ET.Element("package",docType)
 
+	#create metadata element
 	def makeMetaData(self):
 		return ET.Element("metadata",{"xmlns:dc":"http://purl.org/dc/elements/1.1/"})
-		# return ET.Element("metadata")
 		
 	def setMeta(self,tagName,params,text):
 		oldElement = self.meta.find(tagName)
 		oldElement.text = text
 		oldElement.attrib = params
 		
+	#add <dc:dcType id:?>text</dc:dcType> to metadata element
 	def addMetaDC(self,dcType,id,text):
 		params = {}
 		if id != '':
 			params = {"id":id}
 		self.meta.append(createElement("dc:"+dcType,params,text))
-
+	
+	# add <meta property=?>text</meta> to metadata element
 	def addMetaProperty(self,property,text):
 		self.meta.append(createElement("meta",{"property":property},text))
 
 	def addManifestItem(self,name,properties,text=''):
 		self.manifest.append(createElement(name,properties,text))
 	
+	
+	## spine format is <itemref item=itemLoc/>
+	#adds item to spine
 	def addSpineItem(self,name,properties,text=''):
 		self.spine.append(createElement(name,properties,text))
+	
+	#add item to front of spine
+	def addSpineFront(self,name,properties,text=''):
+		self.spine.insert(0,createElement(name,properties,text))
 
+	#returns tree in string form
 	def toString(self):
 		return ET.tostring(self.package,encoding="UTF-8",method="xml")
 		
+#holds container.xml file
 class container:
 	
 	def __init__(self):
@@ -60,21 +73,3 @@ class container:
 		
 	def toString(self):
 		return ET.tostring(self.root,encoding="UTF-8",method="xml")
-
-#create table of contents xhtml
-class toc:
-
-	def __init__(self):
-		self.table = ET.Element("nav",{"id":"toc"})
-		self.list = ET.Element("ol")
-		self.table.append(self.list)
-		
-	def addItem(self,href,title):
-		listEntry = ET.Element("li")
-		link = ET.Element("a",{"href":href})
-		link.text = title
-		listEntry.append(link)
-		self.list.append(listEntry)
-		
-	def toString(self):
-		return ET.tostring(self.table)	
